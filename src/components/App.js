a49053d78e599ffe4cca939378c834141f9c9092
pr3sidente <j.g.detsolis@gmail.com>
2018-05-10 15:44:49 +0300

    Implements a Fish component and iterates over the state.fishes to render the fish comp items

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
 
@@ -32,11 +33,18 @@ class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
+
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>
         </div>
+
         <Order />
+
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+
       </div>
     );
   }
