0a1f1437c2b247698dbdcdc780a85a01fc5b343f
pr3sidente <j.g.detsolis@gmail.com>
2018-05-10 13:13:38 +0300

    Turn header.js into a stateless component plus some minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>Catch
-                    <span className="ofThe">
-                        <div className="of">of</div>
-                        <div className="the">the</div>
-                    </span>
-                      Day
-                </h1>
-                <h3 className="tagline">
-                    <span>Fresh Daily</span>
-                </h3>
-            </header>
-        )
-    }
-}
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <div className="of">of</div>
+        <div className="the">the</div>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
 
 export default Header;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
 class Inventory extends React.Component {
-    render() {
-        return (
-            <div className="inventory">
-                Inventory!!
-            </div>
-        )
-    }
+  render() {
+    return <div className="inventory">Inventory!!</div>;
+  }
 }
 
 export default Inventory;
